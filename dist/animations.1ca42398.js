978f90a4ebad532924dc5a41de46f902cfb8818d
DARAZOUKI <Aussama@chalmers.se>
2024-02-14 22:39:44 +0100

    delet1

--- dist/animations.1ca42398.js
+++ dist/animations.1ca42398.js
@@ -172,7 +172,7 @@ module.exports = reloadCSS;
 var reloadCSS = require('_css_loader');
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./path\\to\\parallax-image.jpg":[["parallax-image.6ed74c45.jpg","scss/partials/path/to/parallax-image.jpg"],"scss/partials/path/to/parallax-image.jpg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -197,7 +197,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54358" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53010" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
